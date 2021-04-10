7037600afd2d012e66a16697b1265d5b3a7d8bc1
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-04-10 13:58:07 +0200

    organize code, add comments

--- utils.py
+++ utils.py
@@ -1,8 +1,21 @@
 import itertools
+import data
+
+users = getattr(data, "users", [])
 
 
-def get_users(users, limit=100, offset=1, sort="id:asc", default=[]):
+def get_users(limit=100, offset=1, sort="id:asc", default=[]):
+    """Get users from data(DB)\n
 
+    Args:\n
+        limit (int, optional): max number of returned users. Defaults to 100.\n
+        offset (int, optional): first user to return (use with limit). Defaults to 1.\n
+        sort (str, optional): the order of the result. \
+            attribute:(asc {ascending} or desc {descending}). Defaults to "id:asc".\n
+        default (list, optional): default value if not found. Defaults to [].\n
+    Returns:\n
+        List[dict]: list of users found\n
+    """
     key, order, *_ = sort.lower().split(':')
     reverse = order == "desc"
     if offset < 1 or limit < 1:
@@ -26,7 +39,15 @@ def get_users(users, limit=100, offset=1, sort="id:asc", default=[]):
     return data
 
 
-def filter_user_by_id(users, id, default={}):
+def filter_user_by_id(id, default={}):
+    """Get user api\n
+
+    Args:\n
+        id (int): user ID\n
+        default (dict, optional): default value if not found. Defaults to {}.\n
+    Returns:\n
+        dict: user found\n
+    """
     found = tuple(
         filter(
             lambda u: u['id'] == id,
