122e744387022ffd77f5be10b39141c096d1ea0b
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-04-10 13:34:09 +0200

    Sort users api result

--- utils.py
+++ utils.py
@@ -1,14 +1,22 @@
 import itertools
 
 
-def get_users(users, limit=100, offset=1, default=[]):
+def get_users(users, limit=100, offset=1, sort="id:asc", default=[]):
+
+    key, order, *_ = sort.lower().split(':')
+    reverse = order == "desc"
     if offset < 1 or limit < 1:
         return default
     limit += offset
     limit = len(users)+1 if limit > len(users) else limit
     data = {
         "next": None,
-        "users": [users[i-1] for i in range(offset, limit)],
+        "users": sorted(
+            [
+                users[i-1] for i in range(offset, limit)
+            ],
+            key=lambda u: u[key], reverse=reverse
+        ),
     }
     # manage next data
     if offset < len(users) and limit <= len(users):
