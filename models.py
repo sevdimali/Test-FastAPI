2c76f5b904136f64945f53ea89a187df0fdfd008
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-04-10 15:39:38 +0200

    Add PartialUser model

--- models.py
+++ models.py
@@ -2,7 +2,17 @@ from pydantic import BaseModel
 from typing import Optional
 
 
-class User(BaseModel):
+class PartialUser(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    is_admin: Optional[bool] = False
+    is_staff: Optional[bool] = False
+    is_active: Optional[bool] = False
+
+
+class User(PartialUser):
+    id: int
     first_name: str
     last_name: str
     email: str
