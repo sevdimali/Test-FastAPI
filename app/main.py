f9d31f3e2d1cae51c2a8d50923171e418afa307a
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-04-17 18:27:10 +0200

    not used but usefull

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 
-from api.api_v1.api import router as api_router
 from api.api_v1.storage.database import Database
+from api.api_v1.api import router as api_router
 
 
 app = FastAPI(
     title="My Super Project",
     description="This is a very fancy project, with auto docs for the API and everything",
     version="1.0.0"
+    # , servers=[
+    #     {"url": "https://stag.example.com", "description": "Staging environment"},
+    #     {"url": "https://prod.example.com", "description": "Production environment"},
+    # ],
+    # root_path="/api/v1",
+    # root_path_in_servers=False,
 )
 
 API_BASE_URL = "/api/v1"
