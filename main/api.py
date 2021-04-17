f9e8b9b8cf136d78a442fadb182977690d4b045f
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-04-17 14:48:13 +0200

    WIP TortoiseORM: delete method

--- main/api.py
+++ main/api.py
@@ -2,15 +2,12 @@ from functools import cache
 
 import uvicorn
 from fastapi import FastAPI
-from tortoise.contrib.fastapi import HTTPNotFoundError
-from tortoise import Tortoise, run_async
-from typing import Optional, List
+from typing import Optional
 
 from utils import API_functools
-from settings import DATABASE_CONFIG
 from baseModels import PartialUser, User
 from models import Person
-from models import Person_Pydantic, PersonIn_Pydantic
+from models import Person_Pydantic
 from database import Database
 app = FastAPI(title="My Super Project",
               description="This is a very fancy project, with auto docs for the API and everything",
@@ -21,7 +18,6 @@ Database.connect(app)
 
 @app.get('/')
 async def index():
-    # re = await p.save()
     return {
         "detail": "Welcome to my API build with Python FastApi",
         "apis": ["/users"],
@@ -127,6 +123,10 @@ async def fix_user(user_id: int, user: PartialUser):
     Returns:\n
         dict: Success operation\n
     """
+    response = {
+        "success": False,
+        "user": {}
+    }
     # await Person.filter(id=user_id).update(**user.dict(exclude_unset=True))
     # return await Person_Pydantic.from_queryset_single(Person.get(id=user_id))
 
@@ -136,15 +136,14 @@ async def fix_user(user_id: int, user: PartialUser):
     see: https://pydantic-docs.helpmanual.io/usage/validators/
     """
     if user_id == 1:
-        return {"detail": "Cannot patch user with ID {user_id}. 🥺"}
+        response['detail'] = "Cannot patch user with ID {user_id}. 🥺"
+        return response
 
     user_found = await Person.get_or_none(id=user_id)
     if user_found is None:
-        return {
-            "success": False,
-            "user": {},
-            "detail": "User with ID {user_id} doesn't exist."
-        }
+        response['detail'] = "User with ID {user_id} doesn't exist."
+        return response
+
     user_updated = user_found.update_from_dict(
         {**user.__dict__, "id": user_found.id,
          "is_admin": user_found.is_admin}
@@ -199,8 +198,8 @@ async def update_user(user_id: int, new_user: User):
     return await Person_Pydantic.from_tortoise_orm(new_user)
 
 
-@ app.delete('/users/{id}')
-def delete_user(id: int):
+@app.delete('/users/{user_id}')
+async def delete_user(user_id: int):
     """Delete a user\n
 
     Args:\n
@@ -209,4 +208,33 @@ def delete_user(id: int):
     Returns:\n
         dict: Success operation\n
     """
-    return []  # user_table.delete_user(id)
+    response = {
+        "success": False,
+        "user": {}
+    }
+    if user_id == 1:
+        response['detail'] = f"Cannot delete user with ID {user_id}. 🥺"
+        return response
+
+    user_found = await Person.get_or_none(id=user_id)
+    return_data = {
+
+    }
+    if not user_found:
+        response['detail'] = f"User with ID {user_id} doesn't exist"
+        return response
+
+    await user_found.delete()
+    print("ID", user_found.id)
+    user_still_there = await Person.exists(id=user_id)
+    print("user still there", user_still_there)
+    if user_still_there:
+        response['detail'] = f"Error while deleting user {user_id}"
+        return response
+
+    response['success'] = True
+    response['user'] = user_found.__dict__
+    del response['user']['_partial']
+    del response['user']['_saved_in_db']
+    response['detail'] = f"User {user_id} delete successfully ⭐"
+    return response
