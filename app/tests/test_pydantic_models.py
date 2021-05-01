3a9333d38faf5634448c85f90eca1323b0bca3e2
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-05-01 19:20:02 +0200

    Add test_user_attributes

--- app/tests/test_pydantic_models.py
+++ app/tests/test_pydantic_models.py
@@ -1,5 +1,28 @@
 from tortoise.contrib import test
+from api.api_v1.models.pydantic import User, PartialUser
 
 
 class TestPydantic(test.TestCase):
-    pass
+    def test_user_attributes(self):
+        user_attributes = (
+            "first_name",
+            "last_name",
+            "email",
+            "avatar",
+            "company",
+            "job",
+            "is_admin",
+            "gender",
+            "date_of_birth",
+            "country_of_birth",
+        )
+        assert User.attributes() == user_attributes
+        partialUser_attributes = (
+            "first_name",
+            "last_name",
+            "email",
+            "avatar",
+            "company",
+            "job",
+        )
+        assert PartialUser.attributes() == partialUser_attributes
