32591591f010e0df8f2d265d177cac7343fd815a
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-05-02 01:29:27 +0200

    Test query_builder

--- app/tests/test_database.py
+++ app/tests/test_database.py
@@ -1,7 +1,7 @@
 import concurrent.futures as futures
 
 from tortoise.contrib import test
-
+from tortoise.query_utils import Q
 from main import app
 from api.api_v1.storage.database import Database
 
@@ -16,3 +16,57 @@ class TestDatabase(test.TestCase):
             )
             # Connection NOK
             assert executor.submit(Database.connect).result() is False
+
+    def test_query_builder(self):
+        value = "john"
+        scenes = [
+            {
+                "attr": "first_name",
+                "expected": [Q(first_name__icontains=value)],
+            },
+            {
+                "attr": "first_nameOrlast_name",
+                "expected": [
+                    Q(
+                        Q(first_name__icontains=value),
+                        Q(last_name__icontains=value),
+                        join_type=Q.OR,
+                    )
+                ],
+            },
+            {
+                "attr": "first_nameANDlast_name",
+                "expected": [
+                    Q(first_name__icontains=value),
+                    Q(last_name__icontains=value),
+                ],
+            },
+            {
+                "attr": "first_nameAndlast_nameAndgenderOremail",
+                "expected": [
+                    Q(first_name__icontains=value),
+                    Q(last_name__icontains=value),
+                    Q(
+                        Q(gender__icontains=value),
+                        Q(email__icontains=value),
+                        join_type=Q.OR,
+                    ),
+                ],
+            },
+            {
+                "attr": "first_nameORlast_nameOrgenderANDemail",
+                "expected": [
+                    Q(
+                        Q(first_name__icontains=value),
+                        Q(last_name__icontains=value),
+                        Q(gender__icontains=value),
+                        join_type=Q.OR,
+                    ),
+                    Q(email_icontains=value),
+                ],
+            },
+        ]
+        for scene in scenes:
+            assert len(Database.query_builder(scene["attr"], value)) == len(
+                scene["expected"]
+            )
