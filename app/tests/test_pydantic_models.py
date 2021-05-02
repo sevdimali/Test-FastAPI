a3d5d0b15b960eac10d65ffc65d205ac0f2fccd6
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-05-02 16:04:14 +0200

    Add test validator between_3_and_50_characters

--- app/tests/test_pydantic_models.py
+++ app/tests/test_pydantic_models.py
@@ -1,4 +1,9 @@
+import pytest
+
+from pydantic import BaseConfig, Field
+from pydantic.fields import ModelField
 from tortoise.contrib import test
+
 from api.api_v1.models.pydantic import User, PartialUser
 
 
@@ -26,3 +31,22 @@ class TestPydantic(test.TestCase):
             "job",
         )
         assert PartialUser.attributes() == partialUser_attributes
+
+    def test_between_3_and_50_characters(self):
+        f = Field(..., min_length=3, max_length=50)
+        field = ModelField(
+            name="name",
+            type_=str,
+            class_validators={},
+            field_info=f,
+            model_config=BaseConfig,
+        )
+        with pytest.raises(ValueError):
+            long_text = "Lorem Ipsum is simply dummy text of\
+                the printing and typesetting industry. "
+            PartialUser.between_3_and_50_characters("He", field=field)
+            PartialUser.between_3_and_50_characters(
+                long_text,
+                field=field,
+            )
+        assert PartialUser.between_3_and_50_characters("Hello") == "Hello"
