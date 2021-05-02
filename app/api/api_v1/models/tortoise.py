028dce7bf8dc30e221541c65744eecc8c5b0a491
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-05-02 15:28:41 +0200

    Remove no pragma cover on __str__ & __repr__

--- app/api/api_v1/models/tortoise.py
+++ app/api/api_v1/models/tortoise.py
@@ -16,12 +16,12 @@ class Person(models.Model):
     date_of_birth = fields.DateField()
     country_of_birth = fields.CharField(max_length=50)
 
-    def __str__(self):  # pragma no cover
+    def __str__(self):
         return "{}(name={} {})".format(
             self.__class__.__name__, self.first_name, self.last_name
         )
 
-    def __repr__(self):  # pragma no cover
+    def __repr__(self):
         return self.__str__()
 
 
