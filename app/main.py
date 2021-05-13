c4cc6500423bb5637c02cd04f58f11867073962d
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-05-14 01:06:44 +0200

    unused code

--- app/main.py
+++ app/main.py
@@ -16,19 +16,7 @@ app = FastAPI(
     title="My Super API",
     description="This is a very fancy project, with auto \
         docs for the API and everything",
-    version="1.0.0",
-    # servers=[
-    #     {
-    #         "url": "https://stag.example.com",
-    #         "description": "Staging environment"
-    #     },
-    #     {
-    #         "url": "https://prod.example.com",
-    #         "description": "Production environment"
-    #     },
-    # ],
-    # root_path=API_BASE_URL,
-    # root_path_in_servers=False,
+    version="1.0.0"
 )
 
 app.add_middleware(CORSMiddleware, **CORS_MIDDLEWARE_CONFIG)
