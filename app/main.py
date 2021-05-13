159bcd643970c974428b5a32b4b5cd7d46de54f2
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-05-13 23:18:18 +0000

    Fixup: format code with Black

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
     title="My Super API",
     description="This is a very fancy project, with auto \
         docs for the API and everything",
-    version="1.0.0"
+    version="1.0.0",
 )
 
 app.add_middleware(CORSMiddleware, **CORS_MIDDLEWARE_CONFIG)
