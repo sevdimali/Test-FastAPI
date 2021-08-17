c95a4b1868592b784668fa95840148706cf4285b
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-08-18 00:48:59 +0200

    Complete router: vote, comment

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 
 from .endpoints import persons
+from .endpoints import comments
+from .endpoints import votes
 
 router = APIRouter()
 
 router.include_router(persons.router, prefix="/users", tags=["users"])
+router.include_router(comments.router, prefix="/comments", tags=["comments"])
+router.include_router(votes.router, prefix="/votes", tags=["votes"])
