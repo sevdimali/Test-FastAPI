88469ed19a53ca5fb16c3f3874ed5f29c7a7496c
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-08-17 15:28:41 +0200

    Add test comment pydantic

---
+++ app/tests/test_comment_pydantic.py
@@ -0,0 +1,28 @@
+import pytest
+
+from pydantic import BaseConfig, Field
+from pydantic.fields import ModelField
+from tortoise.contrib import test
+
+from api.utils import API_functools
+from api.api_v1.models.pydantic import Comment
+
+
+class TestComment(test.TestCase):
+    def test_user_attributes(self):
+        comment_attributes = ("owner", "added", "content")
+        assert API_functools.get_attributes(Comment) == comment_attributes
+
+    def test_as_least_1_character(self):
+        f = Field(..., min_length=1)
+        field = ModelField(
+            name="content",
+            type_=str,
+            class_validators={},
+            field_info=f,
+            model_config=BaseConfig,
+        )
+        with pytest.raises(ValueError):
+            Comment.at_least_1_character("        ", field=field)
+            Comment.at_least_1_character("", field=field)
+        assert Comment.at_least_1_character("First comment") == "First comment"
