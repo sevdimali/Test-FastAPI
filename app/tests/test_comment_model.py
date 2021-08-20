17ba3c9c592436e6bc1a0242fab81df639bef67b
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-08-20 14:18:17 +0200

    Add test get_comments, Fix modules imports

--- app/tests/test_comment_model.py
+++ app/tests/test_comment_model.py
@@ -1,12 +1,16 @@
+from fastapi import status
+from httpx import AsyncClient
 from tortoise.contrib import test
 
-from api.api_v1 import settings
-from api.api_v1.models.tortoise import Person, Comment
-from api.api_v1.models.pydantic import default_content
+from main import app
+from app.api.api_v1 import settings
+from app.api.utils import API_functools
+from app.api.api_v1.models.tortoise import Person, Comment
+from app.api.api_v1.models.pydantic import default_content as lorem
 
 TORTOISE_TEST_DB = getattr(settings, "TORTOISE_TEST_DB", "sqlite://:memory:")
 BASE_URL = "http://127.0.0.1:8000"
-API_ROOT = "/api/v1/users/"
+API_ROOT = "/api/v1/comments/"
 avatar = "https://robohash.org/autdoloremaccusamus.png?size=150x150&set=set1"
 USER_DATA = {
     "is_admin": True,
@@ -25,7 +29,7 @@ USER_DATA = {
 class TestPersonAPi(test.TestCase):
     async def test__str__repr__(self):
         owner = await Person.create(**USER_DATA)
-        comment = await Comment.create(owner=owner, content=default_content)
+        comment = await Comment.create(owner=owner, content=lorem)
 
         expected_repr = "Class({!r})[{!r}]".format(
             comment.__class__.__name__,
@@ -36,3 +40,34 @@ class TestPersonAPi(test.TestCase):
         )
         assert comment.__repr__() == expected_repr
         assert comment.__str__() == expected_str
+
+    async def test_get_comments(self):
+        async with AsyncClient(app=app, base_url=BASE_URL) as ac:
+            response = await ac.get(API_ROOT)
+
+        expected = {"detail": "Not Found", "success": False, "comments": []}
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        assert response.json() == expected
+
+        # Create new User
+        person = await Person.create(**USER_DATA)
+        comment = await Comment.create(owner=person, content=lorem)
+        assert comment.id == 1
+
+        async with AsyncClient(app=app, base_url=BASE_URL) as ac:
+            response = await ac.get(API_ROOT)
+        expected = {
+            "next": None,
+            "previous": None,
+            "comments": [
+                {
+                    "id": comment.id,
+                    "owner_id": person.id,
+                    "added": comment.added.isoformat(),
+                    "content": API_functools.strip_spaces(lorem),
+                }
+            ],
+        }
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == expected
