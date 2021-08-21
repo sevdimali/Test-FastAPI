0e96f5efb8caa08a2738930d67fe90366d2b6926
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-08-21 12:31:39 +0200

    test_get_comments_with_limit_offset

--- app/tests/test_comment_model.py
+++ app/tests/test_comment_model.py
@@ -1,3 +1,5 @@
+import concurrent.futures as futures
+
 from fastapi import status
 from httpx import AsyncClient
 from tortoise.contrib import test
@@ -6,6 +8,7 @@ from main import app
 from app.api.api_v1 import settings
 from app.api.utils import API_functools
 from app.api.api_v1.models.tortoise import Person, Comment
+from app.api.api_v1.storage.initial_data import INIT_DATA
 from app.api.api_v1.models.pydantic import default_content as lorem
 
 TORTOISE_TEST_DB = getattr(settings, "TORTOISE_TEST_DB", "sqlite://:memory:")
@@ -27,6 +30,19 @@ USER_DATA = {
 
 
 class TestPersonAPi(test.TestCase):
+    async def insert_comments(self, comments: list[dict], owners: list[dict]):
+        # Insert data
+        with futures.ProcessPoolExecutor() as executor:
+            for comment, owner in zip(comments, owners):
+                executor.map(
+                    await API_functools._insert_default_data("person", owner)
+                )
+                executor.map(
+                    await API_functools._insert_default_data(
+                        "comment", comment
+                    )
+                )
+
     async def test__str__repr__(self):
         owner = await Person.create(**USER_DATA)
         comment = await Comment.create(owner=owner, content=lorem)
@@ -70,4 +86,76 @@ class TestPersonAPi(test.TestCase):
             ],
         }
         assert response.status_code == status.HTTP_200_OK
+        assert response.json() == expected
+
+    async def test_get_comments_with_limit_offset(self):
+        limit = 4
+        offset = 0
+        comments = sorted(
+            INIT_DATA.get("comment", []), key=lambda c: c["owner"]
+        )[: limit + 4]
+        owners = INIT_DATA.get("person", [])[: limit + 4]
+
+        await self.insert_comments(comments, owners)
+
+        assert await Comment.all().count() == len(comments)
+
+        # Scene 1 get first data, previous=Null
+        async with AsyncClient(app=app, base_url=BASE_URL) as ac:
+            response = await ac.get(
+                API_ROOT, params={"limit": limit, "offset": offset}
+            )
+
+        expected = {
+            "next": f"{API_ROOT}?limit={limit}&offset={limit}",
+            "previous": None,
+            "comments": [
+                {
+                    "id": n,
+                    "added": comment["added"],
+                    "content": comment["content"],
+                    "owner_id": comment["owner"],
+                }
+                for n, comment in enumerate(comments[:limit], start=1)
+            ],
+        }
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == expected
+
+        # Scene 2 get last data, next=Null
+        async with AsyncClient(app=app, base_url=BASE_URL) as ac:
+            response = await ac.get(
+                API_ROOT, params={"limit": limit, "offset": limit}
+            )
+        expected = {
+            "next": None,
+            "previous": f"{API_ROOT}?limit={limit}&offset={offset}",
+            "comments": [
+                {
+                    "id": n,
+                    "added": comment["added"],
+                    "content": comment["content"],
+                    "owner_id": comment["owner"],
+                }
+                for n, comment in enumerate(comments[limit:], start=limit + 1)
+            ],
+        }
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == expected
+
+        limit = 0
+        offset = -1
+        # Test bad limit and offset values
+        async with AsyncClient(app=app, base_url=BASE_URL) as ac:
+            response = await ac.get(
+                API_ROOT, params={"limit": limit, "offset": limit}
+            )
+        expected = {
+            "success": False,
+            "comments": [],
+            "detail": "Invalid values: offset(>=0) or limit(>0)",
+        }
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.json() == expected
